b49781c3eec0d55efcfcba78299a7f02be113682
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2023-06-15 11:12:37 -0300

    feat: Create registration form and apply field validations
    This commit adds the registration form to the application using the FormBuilder from Angular. The form includes fields for ID, name, password, email, gender, role, and isActive.
    
    Each field has specific validations applied:
    - ID requires a minimum length of 5 characters.
    - Name is required.
    - Password is required and must follow the pattern for a secure password: at least one lowercase letter, one uppercase letter, one digit, one special character, and a minimum length of 8 characters.
    - Email is required and must be a valid email address.
    - Gender has a default value of "male".
    - Role is optional and can be left empty.
    - isActive is set to false by default.
    
    This commit ensures that the registration form is properly configured with the necessary validations, providing a solid foundation for user registration functionality.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private builder:FormBuilder){
+
+  }
+registerForm= this.builder.group({
+  id:this.builder.control('',Validators.compose([Validators.required,Validators.minLength(5)])),
+  name:this.builder.control('',Validators.required),
+  password:this.builder.control('',Validators.compose([Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')])),
+  email:this.builder.control('',Validators.compose([Validators.required,Validators.email])),
+  gender:this.builder.control('male'),
+  role:this.builder.control(''),
+  isActive:this.builder.control(false)
+})
+
 }
+
