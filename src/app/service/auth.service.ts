4771b189392eb94424d5ca0ed1d6bfb2eb4b93b3
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2023-06-15 12:37:49 -0300

    feat: Add authentication service with CRUD operations
    This commit introduces the AuthService, which is responsible for handling authentication-related functionalities. The service is injected with the HttpClient to perform HTTP requests.
    
    The following methods are implemented in the AuthService:
    - `getAll()`: Sends a GET request to retrieve all user data from the specified base API endpoint.
    - `getById(id)`: Sends a GET request to retrieve user data by a specific ID from the base API endpoint.
    - `registerData(inputdata)`: Sends a POST request to register user data by sending the input data to the base API endpoint.
    - `updateData(id, inputdata)`: Sends a PUT request to update user data by a specific ID, sending the updated input data to the base API endpoint.
    
    These methods provide the necessary functionality to perform CRUD (Create, Read, Update, Delete) operations on user data.
    
    This commit sets up the foundation for handling authentication-related API requests, allowing for user registration, retrieval, and updating of user data.

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
-  constructor() { }
+export class AuthService {
+  baseAPI = 'http://localhost:3000/user'
+
+  constructor(private http:HttpClient) { }
+
+  //return All Data via Gret Method
+  getAll(){
+    return this.http.get(this.baseAPI);
+  }
+
+  // get recorded by single id
+  getById(id:any){
+    return this.http.get(this.baseAPI+'/'+id);
+  }
+
+  // register User via method
+  registerData (inputdata:any){
+    return this.http.post(this.baseAPI, inputdata);
+  }
+
+  // update user via put method
+  updateData(id:any, inputdata:any){
+    return this.http.put(this.baseAPI+'/'+id,inputdata);
+  }
 }
