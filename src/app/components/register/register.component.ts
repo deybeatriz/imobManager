76c6a4cbc59650ec3c982e5e041d16beb0596ccb
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2023-06-15 21:40:35 -0300

    fix: Adjust rules for validation of register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,39 +1,66 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  constructor(
+    private builder: FormBuilder,
+    private toastr: ToastrService,
+    private service: AuthService,
+    private router: Router
+  ) {}
+  registerForm = this.builder.group({
+    id: this.builder.control(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(5)])
+    ),
+    name: this.builder.control('', Validators.required),
+    password: this.builder.control(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern(
+          '(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+        ),
+      ])
+    ),
+    email: this.builder.control(
+      '',
+      Validators.compose([
+        Validators.required, 
+        Validators.email])
+    ),
+    tel: this.builder.control(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.pattern(
+          '/^(?d{2})?[-.s]?d{4,5}[-.s]?d{4}$/'
+          )
+        ])
+    ),
+    role: this.builder.control(''),
+    isActive: this.builder.control(false),
+  });
 
-  constructor(private builder:FormBuilder, private toastr:ToastrService, private service:AuthService, private router:Router){
-
-  }
-registerForm= this.builder.group({
-  id:this.builder.control('',Validators.compose([Validators.required,Validators.minLength(5)])),
-  name:this.builder.control('',Validators.required),
-  password:this.builder.control('',Validators.compose([Validators.required,Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')])),
-  email:this.builder.control('',Validators.compose([Validators.required,Validators.email])),
-  tel:this.builder.control('',Validators.compose([Validators.required,Validators.pattern('^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$')])),
-  role:this.builder.control(''),
-  isActive:this.builder.control(false)
-})
-
-registration(){
-  if(this.registerForm.valid){
-    this.service.registerData(this.registerForm.value).subscribe((res=>{
-      this.toastr.success('Por favor, entre em contato com um Administrador para habilitar seu acesso', 'Registro realizado com sucesso' );
-      this.router.navigate(['login'])
-    }))
-  }else{
-    this.toastr.warning('Por favor, insira dados válidos"')
+  registration() {
+    if (this.registerForm.valid) {
+      this.service.registerData(this.registerForm.value).subscribe((res) => {
+        this.toastr.success(
+          'Por favor, entre em contato com um Administrador para habilitar seu acesso',
+          'Registro realizado com sucesso'
+        );
+        this.router.navigate(['login']);
+      });
+    } else {
+      this.toastr.warning('Por favor, insira dados válidos"');
+    }
   }
 }
-
-}
-
