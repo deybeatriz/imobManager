ff01a9e875b0bb8827afeca0c666ad1b3b7f960f
Deyse Beatriz <dbfdj.eng20@uea.edu.br>
2023-06-15 23:01:09 -0300

    Revert "bug"
    This reverts commit 0ed5d0757dc5642942e3492abfdbd9411fbc5e60.

--- src/material.module.ts
+++ src/material.module.ts
@@ -4,8 +4,11 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @NgModule({
   exports: [
     MatInputModule,
@@ -15,6 +18,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     MatIconModule,
     MatSidenavModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
   ],
 })
 export class MaterialModule {}
